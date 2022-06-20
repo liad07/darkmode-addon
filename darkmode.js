c127af43bcdb843d09bca9de4f5241550d3a381f
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-20 18:04:03 +0300

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,3 @@
+var style=document.createElement("style")
+style.innerHTML="*{color:white !important;background-color:#26282C !important}"
+document.getElementsByTagName("head")[0].appendChild(style);
